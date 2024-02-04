ae30a39ac348b630695616880a79cb9137600e42
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-02-04 09:44:38 +0630

    add logout page

---
+++ logout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IdP Login</title>
+    <link href="https://riomac.zohodesk.com/portal/api/publicImages/950038000000255201?portalId=edbsn9ea9485e90573df8c3aa7702a5af8fe9dcf22a7ccd284b1d2c50ffabe2acc9f0" type="IMAGE/X-ICON" rel="SHORTCUT ICON">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4 rounded" src="https://info.varonis.com/hubfs/Blog_SAML_BlogHero_202202_V1.png" alt="" width="auto" height="200">
+        <h1 class="display-5 fw-bold text-body-emphasis my-4">IDP Logout </h1>
+        <div class="col-lg-6 mx-auto">
+        <p class="lead mb-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit aliquid aspernatur placeat laborum omnis suscipit mollitia molestias eum sunt, minima nihil fuga rerum odio corrupti eius repellat magnam reprehenderit dolorum.
+            <div class="alert alert-success" role="alert">
+            You're successfully logout. 
+            </div>
+        </p>
+        <a href="https://riomac.zohodesk.com/portal/en/home" class="btn btn-lg btn-primary mt-5">Go to sign in</a>
+    </div>
+
+</body>
+</html>
