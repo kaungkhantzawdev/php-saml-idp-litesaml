3431f2eb733e41a3d100e4520a63946d00f01889
kaungkhantzawjudgify <kaung@judgify.me>
2024-02-05 14:04:22 +0630

    change iocn

--- dashboard.php
+++ dashboard.php
@@ -11,8 +11,8 @@ $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IdP Login</title>
-    <link href="https://riomac.zohodesk.com/portal/api/publicImages/950038000000255201?portalId=edbsn9ea9485e90573df8c3aa7702a5af8fe9dcf22a7ccd284b1d2c50ffabe2acc9f0" type="IMAGE/X-ICON" rel="SHORTCUT ICON">
+    <title>Dashboard</title>
+    <link href="https://www.judgify.me/l/wp-content/uploads/2018/09/judgify-favicon.png" type="IMAGE/X-ICON" rel="SHORTCUT ICON">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
