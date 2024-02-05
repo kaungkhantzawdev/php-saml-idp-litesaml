9da873205ad62add1aa462c6346708e4c61f1cd3
kaungkhantzawjudgify <kaung@judgify.me>
2024-02-05 14:11:16 +0630

    fix logout

--- logout.php
+++ logout.php
@@ -3,23 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IdP Login</title>
-    <link href="https://riomac.zohodesk.com/portal/api/publicImages/950038000000255201?portalId=edbsn9ea9485e90573df8c3aa7702a5af8fe9dcf22a7ccd284b1d2c50ffabe2acc9f0" type="IMAGE/X-ICON" rel="SHORTCUT ICON">
+    <title>Logout</title>
+    <link href="https://www.judgify.me/l/wp-content/uploads/2018/09/judgify-favicon.png" type="IMAGE/X-ICON" rel="SHORTCUT ICON">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
 
-    <div class="px-4 py-5 my-5 text-center">
-        <img class="d-block mx-auto mb-4 rounded" src="https://info.varonis.com/hubfs/Blog_SAML_BlogHero_202202_V1.png" alt="" width="auto" height="200">
-        <h1 class="display-5 fw-bold text-body-emphasis my-4">IDP Logout </h1>
-        <div class="col-lg-6 mx-auto">
+    <div class="px-4 py-5 my-5 text-center mx-auto" style="width:500px">
+        <img class="d-block mb-4 rounded" src="https://www.judgify.me/l/wp-content/uploads/2020/09/Judgify-logo.png" alt="" width="200px" height="auto">
         <p class="lead mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit aliquid aspernatur placeat laborum omnis suscipit mollitia molestias eum sunt, minima nihil fuga rerum odio corrupti eius repellat magnam reprehenderit dolorum.
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-success border-0" role="alert">
             You're successfully logout. 
             </div>
         </p>
-        <a href="https://riomac.zohodesk.com/portal/en/home" class="btn btn-lg btn-primary mt-5">Go to sign in</a>
+        <a href="https://riomac.zohodesk.com/portal" class="btn btn-lg btn-primary mt-5 w-100">Go to sign in</a>
     </div>
 
 </body>
