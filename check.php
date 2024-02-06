c61af940464f770584c584f58db472fd4ac57357
kaungkhantzawjudgify <kaung@judgify.me>
2024-02-06 15:08:30 +0630

    check

--- check.php
+++ check.php
@@ -11,19 +11,19 @@ echo "<pre>";
 /**  Reading the HTTP Request */
 $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
-// $samlResponse = base64_decode($saml_response['SAMLResponse']);
+$samlResponse = base64_decode($request->get('SAMLResponse'));
     
-// // Load the response into a DOMDocument
-// $doc = new DOMDocument();
-// $doc->loadXML($samlResponse);
+// Load the response into a DOMDocument
+$doc = new DOMDocument();
+$doc->loadXML($samlResponse);
 
-// // Extract relevant information (e.g., user attributes)
-// $xpath = new DOMXPath($doc);
-// $xpath->registerNamespace('saml', 'urn:oasis:names:tc:SAML:2.0:assertion');
+// Extract relevant information (e.g., user attributes)
+$xpath = new DOMXPath($doc);
+$xpath->registerNamespace('saml', 'urn:oasis:names:tc:SAML:2.0:assertion');
 // $email = $xpath->query("//saml:Attribute[@Name='Email']/saml:AttributeValue")->item(0)->nodeValue;
 
 
-var_dump($request->get('SAMLResponse'));
+var_dump($xpath);
 die();
     
 
